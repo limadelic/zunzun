f7a49d4b25c2496a74fabd5feb66aaa3f122b230
msuarz <mikemps@gmail.com>
2009-11-08 20:13:20 +0000

    struggling with svn

--- src/App/Presenters/StatusPresenter.cs
+++ src/App/Presenters/StatusPresenter.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Windows;
 using Zunzun.App.Views;
 using Zunzun.Domain;
@@ -11,25 +10,21 @@ namespace Zunzun.App.Presenters {
 
         public TweetService TweetService { get; set; }
 
-        public void Update(Tweet Tweet) {
-            TweetService.UpdateStatus(Tweet);
-        }
+        public void Update(Tweet Tweet) { TweetService.UpdateStatus(Tweet); }
 
         public void Update() {
             if (string.IsNullOrEmpty(View.UpdateText)) return;
-            
+
             Update(Domain.ObjectFactory.NewTweet(View.UpdateText));
             ClearUpdateText();
         }
 
-        private void ClearUpdateText()
-        {
-            View.UpdateText = string.Empty;
-        }
+        void ClearUpdateText() { View.UpdateText = string.Empty; }
 
-        public void ToggleUpdateVisibility()
-        {
-            View.UpdateVisibility = View.UpdateVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        public void ToggleUpdateVisibility() {
+            View.UpdateVisibility = View.UpdateVisibility == Visibility.Visible
+                ? Visibility.Collapsed
+                : Visibility.Visible;
         }
     }
 }
