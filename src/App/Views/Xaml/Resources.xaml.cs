d80d4988cc6bde7bf89b04f2c5ac0addf84dc003
msuarz <mikemps@gmail.com>
2009-11-21 00:48:24 +0000

    ui for RT & D

--- src/App/Views/Xaml/Resources.xaml.cs
+++ src/App/Views/Xaml/Resources.xaml.cs
@@ -9,5 +9,15 @@ namespace Zunzun.App.Views.Xaml {
             var Tweet = (Sender as FrameworkElement).DataContext as Tweet;
             Events.Reply.To(Tweet, Sender);
         }
+        
+        void OnRetweet(object Sender, RoutedEventArgs Args) {
+            var Tweet = (Sender as FrameworkElement).DataContext as Tweet;
+            Events.Retweet.To(Tweet, Sender);
+        }
+        
+        void OnDirectMessage(object Sender, RoutedEventArgs Args) {
+            var Tweet = (Sender as FrameworkElement).DataContext as Tweet;
+            Events.DirectMessage.To(Tweet, Sender);
+        }
     }
 }
